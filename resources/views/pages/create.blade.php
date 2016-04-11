67af9b221392bf366804534d3bdea6c165d46827
Rick Boss <rickeboss@gmail.com>
2016-04-10 21:48:42 -0400

    Upate create project page

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -9,20 +9,19 @@
 @section('mainBody')
 
 <main class="container primary-main">
-    <form class="project-form" action="{{ url('/create') }}" method="post">
+    <form role="form" class="project-form" action="{{ url('/create') }}" method="post" enctype="multipart/form-data">
         {!! csrf_field() !!}
+
         <h5>Create Project</h5>
-        <fieldset class="create-project">
-            <label>
-                Title
-                <input type="text" name="title">
-            </label>
-            <label>
-                Description
-                <input type="text" name="description">
-            </label>
-            <input type="submit" value="Submit">
-        </fieldset>
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input class="form-control" type="text" id="title" name="title" placeholder="Your Project's Title">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" rows="3" cols="50" placeholder="About your project"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </main>
 
