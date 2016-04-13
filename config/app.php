b3d89ac51cfed85bb7dbd50728ab517b59334fc8
Rick Boss <rickeboss@gmail.com>
2016-04-13 03:31:27 -0400

    Add Collective\Html

--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -202,6 +203,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
