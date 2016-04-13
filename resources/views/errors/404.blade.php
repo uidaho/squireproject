83b6bdf833483bd2aa38298a5f98ab4f4a114e9a
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-12 22:01:25 -0400

    add custom 404 error page for issue #62

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Not Found (404) | The Squire Project</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <img alt="404" src="/images/errors/404.jpg">
+                <div class="title">Page Not Found (404)</div>
+                <strong>Unfortunately, the page you requested does not exist on our site.</strong>
+            </div>
+        </div>
+    </body>
+</html>
