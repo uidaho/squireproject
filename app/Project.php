9a00e7807cb1bfa0f8438ed55d11ce789843aa05
Rick Boss <rickeboss@gmail.com>
2016-04-13 03:33:28 -0400

    Add verification to create project form
    - Form has verification
    - Refactor to user custom request
    - Add js to form that shows length requirements as user is typing
    - Add error messages to create project form

--- app/Project.php
+++ app/Project.php
@@ -9,4 +9,20 @@ class Project extends Model
     protected $fillable = [
         'title', 'author', 'description', 'body', 'created_at', 'updated_at'
     ];
+
+    public static function attributeLengths()
+    {
+        return [
+            'title' => Project::minMaxHelper(2, 50),
+            'description' => Project::minMaxHelper(10, 100),
+            'project-body' => Project::minMaxHelper(100, 65535)
+        ];
+    }
+
+    private static function minMaxHelper($min, $max) {
+        return [
+            'min' => $min,
+            'max' => $max
+        ];
+    }
 }
