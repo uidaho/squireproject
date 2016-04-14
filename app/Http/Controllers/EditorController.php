6d745e5663be8acbab638951bcfdbe0f12bb0146
Backburn <backburn@hotmail.com>
2016-04-14 12:12:17 -0700

    no getOrFail i guess :(

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -38,7 +38,7 @@ class EditorController extends Controller
      */
     public function listFiles($projectname)
     {
-        $files = File::where('projectname', $projectname)->findOrFail();
+        $files = File::where('projectname', $projectname)->get();
 
         return view('editor.list', ['files' => $files]);
     }
