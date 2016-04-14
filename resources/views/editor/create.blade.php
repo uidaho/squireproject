0d109538297d69f22765c6105f57629dc48949c7
Backburn <backburn@hotmail.com>
2016-04-13 19:56:40 -0700

    missing curlys

--- resources/views/editor/create.blade.php
+++ resources/views/editor/create.blade.php
@@ -23,14 +23,14 @@
                 <label for="title">Name</label>
                 <input class="form-control" type="text" id="filename" name="filename" placeholder="filename.txt">
                 @foreach ($errors->get('filename') as $error)
-                    <li>$error</li>
+                    <li>{{$error}}</li>
                 @endforeach
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
                 <input class="form-control" type="text" id="description" name="description" placeholder="A short description" maxlength="100" size="88" height="2">
                 @foreach ($errors->get('description') as $error)
-                    <li>$error</li>
+                    <li>{{$error}}</li>
                 @endforeach
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
