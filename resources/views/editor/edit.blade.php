9491da8a78fd39a518614387c3ae274c07f77517
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-13 23:05:30 -0700

    hash user color from username

--- resources/views/editor/edit.blade.php
+++ resources/views/editor/edit.blade.php
@@ -57,6 +57,22 @@
             return "00000".substring(0, 6 - c.length) + c;
         }
         function init() {
+            var StringToColor = (function(){
+                var instance = null;
+                return {
+                    next: function stringToColor(str) {
+                        if(instance === null) {
+                            instance = {};
+                            instance.stringToColorHash = {};
+                            instance.nextVeryDifferntColorIdx = 0;
+                            instance.veryDifferentColors = ["#000000","#00FF00","#0000FF","#FF0000","#01FFFE","#FFA6FE","#FFDB66","#006401","#010067","#95003A","#007DB5","#FF00F6","#FFEEE8","#774D00","#90FB92","#0076FF","#D5FF00","#FF937E","#6A826C","#FF029D","#FE8900","#7A4782","#7E2DD2","#85A900","#FF0056","#A42400","#00AE7E","#683D3B","#BDC6FF","#263400","#BDD393","#00B917","#9E008E","#001544","#C28C9F","#FF74A3","#01D0FF","#004754","#E56FFE","#788231","#0E4CA1","#91D0CB","#BE9970","#968AE8","#BB8800","#43002C","#DEFF74","#00FFC6","#FFE502","#620E00","#008F9C","#98FF52","#7544B1","#B500FF","#00FF78","#FF6E41","#005F39","#6B6882","#5FAD4E","#A75740","#A5FFD2","#FFB167","#009BFF","#E85EBE"];
+                        }
+                        if(!instance.stringToColorHash[str])
+                            instance.stringToColorHash[str] = instance.veryDifferentColors[instance.nextVeryDifferntColorIdx++];
+                        return instance.stringToColorHash[str];
+                    }
+                }
+            })();
             var userId = '{{$userid}}';
             var firepadRef = new Firebase('https://radiant-torch-8044.firebaseio.com/{{$file->projectname}}/{{$file->filename}}');
             var codeMirror = CodeMirror(document.getElementById('firepad-container'), {
@@ -69,7 +85,7 @@
                 mode: 'text/x-java'});
             var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
                 userId: userId,
-                userColor: intToRGB(hashCode(userId))});
+                userColor: StringToColor.next(userId)});
             var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'), document.getElementById('userlist'), userId);
             firepad.on('ready', function() {
                 if (firepad.isHistoryEmpty()) {
