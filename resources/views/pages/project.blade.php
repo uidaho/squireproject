ece87e6a7b8cf49b1d05dba0e31427182762707e
brec9824 <brec9824@vandals.uidaho.edu>
2016-04-13 22:11:59 -0400

    Fixed code my last commit overwrote in last fetch
    Added auth check section back into project.blade.php

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -28,32 +28,6 @@
                     </a>
                 </div>
             @endif
-        </div>
-    </main>
-@stop
-
-
-@section('mainBody')
-    <main class="container project-page">
-        <div class="row">
-            <div class="col-1-2">
-                <img src="/images/projects/product{{ $project->id }}.jpg" alt="Project Image" border="1px">
-            </div>
-            <div class="col-1-2">
-                <h5>{{ $project->title }} by {{ $project->author  }}</h5>
-                <h4>{{ $project->description }}</h4>
-            </div>
-            <div class="project-page-elements">
-                <pre style="white-space: pre-wrap">{{ $project->body }}</pre>
-            </div>
-            @if (Auth::check() && Auth::user()->username == $project->author)
-                <div class="project-page-elements">
-                    <a href="/delete-project/{{ $project->id }}">
-                        <button type="button" id="delete">Delete</button>
-                    </a>
-                </div>
-            @endif
-
 
             <div class="col-md-6 col-md-offset-3">
                 <br>
