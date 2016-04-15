3df8f7b4833a8941a0a65f67f7dd361abc7ce6ee
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-14 23:45:05 -0700

    bootstrapify project page

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -1,31 +1,23 @@
 @extends('layouts.main_layout')
 
 @section('head')
-
     <title>{{ $project->title }}</title>
-
 @stop
 
 @section('mainBody')
-    <main class="container project-page">
-        <div class="row">
-            <div class="col-1-2">
-                <img src="/images/projects/product{{ $project->id }}.jpg" alt="Project Image" border="1px">
-            </div>
-            <div class="col-1-2">
-                <h5>{{ $project->title }} by {{ $project->author  }}</h5>
-                <h4>{{ $project->description }}</h4>
-            </div>
-            <div class="project-page-elements">
+    <div class="row">
+        <div class="col-md-12">
+            <img src="/images/projects/product{{ $project->id }}.jpg" alt="Project Image" border="1px">
+            <h5>{{ $project->title }} by {{ $project->author  }}</h5>
+            <h4>{{ $project->description }}</h4>
+            <div class="jumbotron well">
                 <pre style="white-space: pre-wrap">{{ $project->body }}</pre>
             </div>
             @if (Auth::check() && Auth::user()->username == $project->author)
-            <div class="project-page-elements">
-                <a href="/delete-project/{{ $project->id }}">
-                    <button type="button" id="delete">Delete</button>
-                </a>
-            </div>
+            <a href="/delete-project/{{ $project->id }}">
+                <button type="button" id="delete">Delete</button>
+            </a>
             @endif
         </div>
-    </main>
+    </div>
 @stop
