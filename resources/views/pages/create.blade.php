aeca814f555a13c01392660ac78fbf0af6d20fa7
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-14 23:36:11 -0700

    bootstrapify create project page

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,39 +1,60 @@
 @extends('layouts.main_layout')
 
 @section('head')
-
     <title>Create Project</title>
-
 @endsection
 
 @section('mainBody')
-
-<main class="container primary-main row">
-    <form role="form" class="project-form" action="{{ url('/create') }}" method="post" enctype="multipart/form-data">
-        {!! csrf_field() !!}
-
+<div class="row">
+    <div class="col-md-12">
         <h5>Create Project</h5>
-        <div class="form-group">
-            <label for="thumbnail">Thumbnail</label>
-            <input class="form-control" type="file" id="thumbnail" name="thumbnail">
-        </div>
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input class="form-control" type="text" id="title" name="title" placeholder="Your project's title">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input class="form-control" type="text" id="description" name="description" placeholder="A short description" maxlength="100" size="88" height="2">
-        </div>
-        <div class="form-group">
-            <label for="project-body">About The Project</label>
-            <textarea class="form-control" id="project-body" name="project-body" rows="15" placeholder="All the details of your project"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <a href="/projectfinder">
-            <button type="button" class="btn btn-danger">Cancel</button>
-        </a>
-    </form>
-</main>
-
+        <form role="form" action="{{ url('/create') }}" method="post" enctype="multipart/form-data">
+            {!! csrf_field() !!}
+            <div class="form-group">
+                <label for="thumbnail">
+                    Thumbnail image
+                </label>
+                <input type="file" id="thumbnail" />
+                <!-- <p class="help-block">
+                    Error here
+                </p> -->
+            </div>
+            <div class="form-group">
+                <label for="title">
+                    Title
+                </label>
+                <input type="text" class="form-control" id="title" />
+                <!-- <p class="help-block">
+                    Error here
+                </p> -->
+            </div>
+            <div class="form-group">
+                <label for="description">
+                    Description
+                </label>
+                <input type="text" class="form-control" id="description" maxlength="100" size="88" height="2" />
+                <!-- <p class="help-block">
+                    Error here
+                </p> -->
+            </div>
+            <div class="form-group">
+                <label for="project-body">
+                    About The Project
+                </label>
+                <textarea class="form-control" id="project-body" rows="15" placeholder="All the details of your project"></textarea>
+                <!-- <p class="help-block">
+                    Error here
+                </p> -->
+            </div>
+            <button type="submit" class="btn btn-default">
+                Submit
+            </button>
+            <a href="/projectfinder">
+                <button type="button" class="btn btn-danger">
+                    Cancel
+                </button>
+            </a>
+        </form>
+    </div>
+</div>
 @stop
