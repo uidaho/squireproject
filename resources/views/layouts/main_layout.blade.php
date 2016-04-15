74281775ae09a83944de916c8568abb0e91765c8
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-14 20:34:53 -0700

    disable old css

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -6,7 +6,7 @@
 
         @yield('head')
 
-        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+        <!-- <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}"> -->
         <link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
         <!-- bootstrap and theme-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
