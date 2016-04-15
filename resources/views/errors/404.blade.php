7ec649191f7c6421a34a7666fbf8af0c42976d03
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-15 00:11:35 -0700

    bladeify 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,32 +2,16 @@
 
 @section('head')
     <title>Page Not Found (404) | The Squire Project</title>
-
-    <style>
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
 @stop
 
 @section('mainBody')
-    <div class="container">
-        <div class="content">
-            <img alt="404" src="/images/errors/404.jpg">
-            <div class="title">Page Not Found (404)</div>
-            <strong>Unfortunately, the page you requested does not exist on our site.</strong>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <img alt="404" src="/images/errors/404.jpg">
+                <div class="title">Page Not Found (404)</div>
+                <strong>Unfortunately, the page you requested does not exist on our site.</strong>
+            </div>
         </div>
     </div>
 @stop
