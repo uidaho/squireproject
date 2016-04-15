41cde807c886df8fedd3cbc6c73a4409c7a856f2
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-15 00:24:50 -0700

    fix empty list

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -41,7 +41,7 @@ class EditorController extends Controller
     {
         $files = File::where('projectname', $projectname)->get();
 
-        if ($files == null) {
+        if (!$files) {
             return redirect('/editor/create/$projectname');
         }
 
