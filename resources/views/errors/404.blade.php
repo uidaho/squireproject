6b78ab6d87fc94b6dcb25fdeade8c6e43ee0a00d
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-15 00:07:03 -0700

    bladeify 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,49 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Page Not Found (404) | The Squire Project</title>
+@extends('layouts.main_layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('head')
+    <title>Page Not Found (404) | The Squire Project</title>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
+@stop
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <img alt="404" src="/images/errors/404.jpg">
-                <div class="title">Page Not Found (404)</div>
-                <strong>Unfortunately, the page you requested does not exist on our site.</strong>
-            </div>
+@section('mainBody')
+    <div class="container">
+        <div class="content">
+            <img alt="404" src="/images/errors/404.jpg">
+            <div class="title">Page Not Found (404)</div>
+            <strong>Unfortunately, the page you requested does not exist on our site.</strong>
         </div>
-    </body>
-</html>
+    </div>
+@stop
+
+
