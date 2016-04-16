15d6862d6d2a9e5012872e637b295173cb6533da
Rick Boss <rickeboss@gmail.com>
2016-04-16 18:22:53 -0400

    Update to Yeti theme

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -7,48 +7,89 @@
 @endsection
 
 @section('mainBody')
-<div class="row">
+<main class="container primary-main row">
     <div class="col-md-12">
-        <h5>Create Project</h5>
-        <form role="form" action="{{ url('/project/create') }}" method="post" enctype="multipart/form-data">
-            {!! csrf_field() !!}
+
+    <form class="form-horizontal" role="form" action="{{ url('/project/create') }}" method="post" enctype="multipart/form-data">
+        {!! csrf_field() !!}
+
+        <fieldset>
+            <legend>Create Project</legend>
             <div class="form-group">
-                <label for="thumbnail">
-                    Thumbnail image
-                </label>
-                <input class="form-control" type="file" id="thumbnail" name="thumbnail" value="{{old('thumbnail')}}">
-                @if ($errors->has('thumbnail'))
-                    <p class="help-block">{{ $errors->first('thumbnail') }}</p>
+                @if($errors->has('thumbnail'))
+                    <div class="has-error">
                 @endif
+
+                <label for="thumbnail" class="col-lg-2 control-label">Thumbnail</label>
+                <div class="col-lg-10">
+                    <input class="form-control" type="file" id="thumbnail" name="thumbnail" value="{{ old('thumbnail') }}}">
+                </div>
+
+                @if($errors->has('thumbnail'))
+                    <label class="control-label col-lg-10">
+                        {{ $errors->first('thumbnail') }}
+                    </label>
+                    </div>
+                @endif
+
             </div>
+
             <div class="form-group">
-                <label for="title">
+                @if($errors->has('title'))
+                    <div class="has-error">
+                @endif
+                <label for="title" class="col-lg-2 control-label">
                     Title
                 </label>
-                <input class="form-control" type="text" id="title" name="title" placeholder="Your project's title" value="{{old('title')}}"
-                       minlength="{{ \App\Project::attributeLengths()['title']['min'] }}" maxlength="{{ \App\Project::attributeLengths()['title']['max'] }}" onkeyup="writeCharCount('title')">
-            @if ($errors->has('title'))
-                    <p class="help-block">{{ $errors->first('title') }}</p>
+                <div class="col-lg-10">
+                    <input class="form-control" type="text" id="title" name="title" value="{{old('title')}}">
+                </div>
+                @if($errors->has('title'))
+                    <label class="control-label col-lg-10">
+                        {{ $errors->first('title') }}
+                    </label>
+                    </div>
                 @endif
             </div>
+
             <div class="form-group">
-                <label for="description">
+                @if($errors->has('description'))
+                    <div class="has-error">
+                @endif
+
+                <label for="description" class="col-lg-2 control-label">
                     Description
                 </label>
-                <input class="form-control" type="text" id="description" name="description" placeholder="A short description" value="{{old('description')}}" size="88" height="2"
-                       minlength="{{ \App\Project::attributeLengths()['description']['min'] }}" maxlength="{{ \App\Project::attributeLengths()['description']['max'] }}" onkeyup="writeCharCount('description')">
-                @if ($errors->has('description'))
-                    <p class="help-block">{{ $errors->first('description') }}</p>
+                <div class="col-lg-10">
+                    <input class="form-control" type="text" id="description" name="description" value="{{ old('description') }}">
+                </div>
+
+                @if($errors->has('description'))
+                    <label class="control-label col-lg-10">
+                        {{ $errors->first('description') }}
+                    </label>
+                    </div>
                 @endif
             </div>
+
             <div class="form-group">
-                <label for="project-body">
+                @if($errors->has('project-body'))
+                    <div class="has-error">
+                @endif
+
+                <label for="project-body" class="col-lg-10 control-label">
                     About The Project
                 </label>
-                <textarea class="form-control" id="project-body" name="project-body" rows="15" placeholder="All the details of your project"
-                          minlength="{{ \App\Project::attributeLengths()['project-body']['min'] }}" maxlength="{{ \App\Project::attributeLengths()['project-body']['max'] }}" onkeyup="writeCharCount('project-body')">{{old('project-body')}}</textarea>
-                @if ($errors->has('project-body'))
-                    <p class="help-block">{{ $errors->first('project-body') }}</p>
+                <div class="col-lg-10">
+                    <textarea class="form-control" id="project-body" name="project-body"
+                              minlength="{{ \App\Project::attributeLengths()['project-body']['min'] }}" maxlength="{{ \App\Project::attributeLengths()['project-body']['max'] }}" onkeyup="writeCharCount('project-body')"> {{ old('project-body') }} </textarea>
+                </div>
+
+                @if($errors->has('project-body'))
+                    <label class="control-label col-lg-10">
+                        {{ $errors->first('project-body') }}
+                    </label>
+                    </div>
                 @endif
             </div>
             <button type="submit" class="btn btn-default">
@@ -61,5 +102,5 @@
             </a>
         </form>
     </div>
-</div>
+</main>
 @stop
