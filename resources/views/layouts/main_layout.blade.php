60bba63c825e02fd4710ec84248d649004396e93
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-16 03:51:01 -0400

    set current page active in breadcrumb

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -101,7 +101,7 @@
                             <a href="/">Home</a>
                         </li>
                         @for($i = 1; $i <= count(Request::segments()); $i++)
-                            <li>
+                            <li @if($i == count(Request::segments())) class="active" @endif>
                                 <a href="@for($j = 1; $j <= $i; $j++)/{{ Request::segment($j) }}@endfor">{{ Request::segment($i) }}</a>
                             </li>
                         @endfor
