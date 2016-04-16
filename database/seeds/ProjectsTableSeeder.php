cd00985909a13e6b9a604bb601d0b344a43f0ba2
brec9824 <brec9824@vandals.uidaho.edu>
2016-04-16 16:31:13 -0400

    Removed commented out code

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -15,26 +15,16 @@ class ProjectsTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        //Creates just projects
+        //Creates projects
         for ($i = 0; $i < 1; $i++)
         {
             factory(Project::class, 1)->create();
+            //factory(Project::class, 'selfContained', 1)->create();                                        //Use when wanting to create connected users with the project
             $newestID = DB::table('projects')->orderBy('created_at', 'desc')->first()->id;                  //Get the project that was just created's id
             $imageURL = $faker->imageUrl(640, 480, 'technics');                                             //Get a new image
             $data = file_get_contents($imageURL);
             $new = base_path() . '/public/images/projects/' . 'product' . $newestID . '.jpg';
             file_put_contents($new, $data);
         }
-
-        //Creates projects and connected users
-        /*for ($i = 0; $i < 1; $i++)
-        {
-            factory(Project::class, 'selfContained', 1)->create();
-            $newestID = DB::table('projects')->orderBy('created_at', 'desc')->first()->id;                  //Get the project that was just created's id
-            $imageURL = $faker->imageUrl(640, 480, 'technics');                                             //Get a new image
-            $data = file_get_contents($imageURL);
-            $new = base_path() . '/public/images/projects/' . 'product' . $newestID . '.jpg';
-            file_put_contents($new, $data);                                                                 //Store new image
-        }*/
     }
 }
