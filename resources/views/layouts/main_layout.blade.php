d8030dadecbf0a4005a76df87d1c3f02779e2309
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-16 03:46:32 -0400

    add breadcrumb to all pages

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -96,6 +96,16 @@
                             </ul>
                         </div>
                     </nav>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        @for($i = 1; $i <= count(Request::segments()); $i++)
+                            <li>
+                                <a href="@for($j = 1; $j <= $i; $j++)/{{ Request::segment($j) }}@endfor">{{ Request::segment($i) }}</a>
+                            </li>
+                        @endfor
+                    </ul>
                 </div>
             </div>
             <!-- CONTENT -->
