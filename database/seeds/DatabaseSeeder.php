46de2498b9eaf9b547fa49d1a62117f67e890dad
brec9824 <brec9824@vandals.uidaho.edu>
2016-04-16 15:34:31 -0400

    Seeders for users and projects
    The database can now be seeded with php artisan db:seed. Model factory has functions for creating just a specific class while some create its class plus its connected classes. Multiple seeders were added that contain multiple call options. Either self contained or non self contained.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\User;
+use App\Project;
+use App\ProjectComment;
 use Illuminate\Database\Seeder;
+use Faker\Generator;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);                         //Seed the users table
+        $this->call(ProjectsTableSeeder::class);                      //Seed the projects table
     }
 }
