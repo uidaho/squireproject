818875001379577beeb4c9807f9a3b0d9e9cb5d0
Brandon Jank <jank6275@vandals.uidaho.edu>
2016-04-16 01:01:38 -0400

    cleanup routes and editor file listing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,27 +11,23 @@
 |
 */
 
-/*-------------------------
- * Routes Authentication
- * ------------------------
- */
 
-// Authentication Routes...
+/*-----------------------*
+ *  Auth Controller      *
+ *-----------------------*/
 Route::get('login', 'Auth\AuthController@showLoginForm');
 Route::post('login', 'Auth\AuthController@login');
 Route::post('loginemail', 'Auth\AuthControllerEmail@login');
 Route::get('logout', 'Auth\AuthController@logout');
-
-// Registration Routes...
 Route::get('register', 'Auth\AuthController@showRegistrationForm');
 Route::post('register', 'Auth\AuthController@register');
-
-// Password Reset Routes...
 Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
 Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 Route::post('password/reset', 'Auth\PasswordController@reset');
 
-//Routes to the different html pages
+/*-----------------------*
+ *  Pages Controller     *
+ *-----------------------*/
 Route::get('/', 'PagesController@home');
 Route::get('about', 'PagesController@about');
 
@@ -50,13 +46,31 @@ Route::get('project/view/{project}', 'ProjectController@view');
 Route::get('project/{project}', 'ProjectController@view'); // overloads go last.
 
 /*-----------------------*
- *  Editor Controller   *
+ *  Editor Controller    *
  *-----------------------*/
 Route::get('editor/create/{projectname}', 'EditorController@createView');
 Route::post('editor/create/{projectname}', 'EditorController@create');
-Route::get('editor/delete/{projectname}/{filename}', 'EditorController@delete');
+Route::get('editor/import/{projectname}', 'EditorController@importView');
+Route::post('editor/import/{projectname}', 'EditorController@import');
+Route::get('editor/export/{projectname}', 'EditorController@exportView');
+Route::post('editor/export/{projectname}', 'EditorController@export');
+Route::get('editor/rename/{projectname}/{filename}', 'EditorController@renameView');
+Route::post('editor/rename/{projectname}/{filename}', 'EditorController@rename');
+Route::get('editor/delete/{projectname}/{filename}', 'EditorController@deleteView');
+Route::post('editor/delete/{projectname}/{filename}', 'EditorController@delete');
 Route::get('editor/edit/{projectname}/{filename}', 'EditorController@editFile');
 Route::get('editor/{projectname}/{filename}', 'EditorController@editFile');
 Route::get('editor/list/{projectname}', 'EditorController@listFiles');
 Route::get('editor/{projectname}', 'EditorController@listFiles');
 Route::get('editor', 'EditorController@index');
+
+/*-----------------------*
+ *  Profile Controller   *
+ *-----------------------*/
+ 
+ 
+ /*-----------------------*
+  *  Settings Controller  *
+  *-----------------------*/
+ 
+ 
--- resources/views/editor/list.blade.php
+++ resources/views/editor/list.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main_layout')
 
 @section('head')
-    <title>{{$files[0]->projectname}} Files | The Squire Project</title>
+    <title>{{$files[0]->projectname}} Project Files | The Squire Project</title>
 @endsection
 
 @section('mainBody')
-<main class="primary-main row project-main">
-    <section class="container">
+<div class="row">
+    <div class="col-md-12">
         <!-- Search -->
         <ul class="project-search">
             <li><input class="search-textbox" type="text" name="searchName" placeholder="Search"></li><!--
@@ -19,21 +19,34 @@
             <option value="created">Created</option>
             <option value="modified">Modified</option>
         </select>
-    </section>
-
-    <!-- Files -->
-    <section class="grid">
-        @foreach($files as $file)
-            <div class="col-1-3 project-teaser">
-                <a href="/editor/edit/{{$file->projectname}}/{{$file->filename}}">
-                    <h4>{{$file->filename}}</h4>
-                    <p>Description: {{$file->type}}</p>
-                    <p>Created by: {{$file->creator}}</p>
-                    <p>Created: {{$file->created_at}}</p>
-                    <p>Updated: {{$file->updated_at}}</p>
-                </a>
-            </div>
-        @endforeach
-    </section>
-</main>
+        <!-- Files -->
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Filename</th>
+                  <th>Type</th>
+                  <th>Description</th>
+                  <th>Creator</th>
+                  <th>Created at</th>
+                  <th>Updated at</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{{$i = 1}}}
+                @foreach($files as $file)
+                    <tr>
+                        <td>{{ $i++ }}</td>
+                        <td><a href="/editor/edit/{{$file->projectname}}/{{$file->filename}}">{{$file->filename}}</a></td>
+                        <td>{{$file->type}}</td>
+                        <td>{{$file->description}}</td>
+                        <td><a href="/profile/view/{{$file->creator}}">{{$file->creator}}</td>
+                        <td>{{$file->created_at}}</td>
+                        <td>{{$file->updated_at}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @stop
