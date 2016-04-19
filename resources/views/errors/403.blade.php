13f5f0457fbc6613026e7812668c3b35f9318b34
brec9824 <brec9824@users.noreply.github.com>
2016-04-18 18:36:12 -0700

    Added 403 error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main_layout')
+
+@section('head')
+    <title>Forbidden (403) | The Squire Project</title>
+@stop
+
+@section('mainBody')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="">
+                <img alt="403" src="/images/errors/403.jpg">
+                <div class="title">Forbidden (403)</div>
+                <strong>Turn back now.</strong>
+            </div>
+        </div>
+    </div>
+@stop
