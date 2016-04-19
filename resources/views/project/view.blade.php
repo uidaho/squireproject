a899dc238113efd7e46e3d9ebf09ea3fb4fbdd96
Brandon Jank <backburn@users.noreply.github.com>
2016-04-18 21:13:15 -0700

    fix delete path to match routes for issue #83 (#90)
    * fix delete path to match routes for issue #83
    
    * use project->title instead of slug for #83

--- resources/views/project/view.blade.php
+++ resources/views/project/view.blade.php
@@ -18,7 +18,7 @@
                     {{ $project->description }}
                 </div>
                 @if (Auth::check() && Auth::user()->username == $project->author)
-                    <a href="{{ $project->getSlug() }}/delete">
+                    <a href="/project/delete/{{ $project->title }}">
                         <button type="button" id="delete">Delete</button>
                     </a>
                 @endif
