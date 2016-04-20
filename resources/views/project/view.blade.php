325b794140aa47898e1b5e52bc9567fc4cf811e3
Rick Boss <rickeboss@gmail.com>
2016-04-19 20:42:41 -0400

    Update view project page layout

--- resources/views/project/view.blade.php
+++ resources/views/project/view.blade.php
@@ -6,32 +6,30 @@
 
 @section('mainBody')
     <div class="row">
-        <div class="col-md-2">
-            <div class="panel-body">
-                <div class="fallback-image">
-                    <div class="project-image" style="background-image: url({{ $project->getImagePath() }});"></div>
-                </div>
-                <hr/>
-                <span class="label label-default project-members">n+1 Members</span>
-                <h4>{{ $project->title }}</h4>
-                <div class="project-description">
-                    {{ $project->description }}
-                </div>
-                <div>
-                    {{ $project->body }}
-                </div>
-                <br>
-                @if (Auth::check())
-                    <a href="/editor/{{ $project->getSlugFriendlyTitle() }}">
-                        <button class="btn btn-default" id="view-files">View Files</button>
-                    </a>
-                    @if (Auth::user()->username == $project->author)
-                        <a href="/project/delete/{{ $project->getSlugFriendlyTitle() }}">
-                            <button class="btn btn-danger" id="delete">Delete</button>
-                        </a>
-                    @endif
-                @endif
+        <div class="panel-body">
+            <div class="fallback-image">
+                <div class="project-image" style="background-image: url({{ $project->getImagePath() }});"></div>
             </div>
+            <hr/>
+            <span class="label label-default project-members">n+1 Members</span>
+            <h4>{{ $project->title }}</h4>
+            <div class="project-description">
+                {{ $project->description }}
+            </div>
+            <div>
+                {{ $project->body }}
+            </div>
+            <br>
+            @if (Auth::check())
+                <a href="/editor/{{ $project->getSlugFriendlyTitle() }}">
+                    <button class="btn btn-default" id="view-files">View Files</button>
+                </a>
+                @if (Auth::user()->username == $project->author)
+                    <a href="/project/delete/{{ $project->getSlugFriendlyTitle() }}">
+                        <button class="btn btn-danger" id="delete">Delete</button>
+                    </a>
+                @endif
+            @endif
         </div>
     </div>
 @stop
