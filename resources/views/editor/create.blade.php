4afa9ff8f4da327efb8fa29b4b1a98b8db338eb1
Rick Boss <rickeboss@gmail.com>
2016-04-21 14:51:25 -0400

    Fix Cancel buttons
    Cancel buttons on file and project create pages now redirect back to the last page. Fixes #109

--- resources/views/editor/create.blade.php
+++ resources/views/editor/create.blade.php
@@ -57,7 +57,9 @@
             </div>
             <div class="form-group">
               <div class="col-lg-10 col-lg-offset-2">
-                <button type="reset" class="btn btn-default">Cancel</button>
+                <a href="{{ back()->getTargetUrl() }}">
+                    <button type="button" class="btn btn-danger">Cancel</button>
+                </a>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </div>
             </div>
