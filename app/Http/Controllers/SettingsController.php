6459b8860b9a98cb4bd477f29794a3fc33433bbe
John Snevily <snev7821@vandals.uidaho.edu>
2016-04-20 18:13:53 -0700

    Skeleton for SettingsController class  - John and Eric

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -8,5 +8,29 @@ use App\Http\Requests;
 
 class SettingsController extends Controller
 {
-    //
+    public function editSettings()
+    {
+        if (Auth::user()->guest)
+        {
+            return redirect(login);
+        }
+
+        else
+        {
+            openSettings(Auth::user()->id);
+        }
+    }
+
+
+    public function openSettings(&$string)
+    {
+
+    }
+    
+    public function applyChanges()
+
+    {
+
+    }
+
 }
