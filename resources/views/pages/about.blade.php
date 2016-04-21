9bc8286f96af79d8eb045f472d380138828f5596
brec9824 <brec9824@vandals.uidaho.edu>
2016-04-21 11:00:45 -0400

    Fixed change in about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,11 +9,12 @@
         <li>
             <a href="/">Home</a>
         </li>
-        @for($i = 1; $i <= count(Request::segments()); $i++)
-            <li @if($i == count(Request::segments())) class="active" @endif>
-                <a href="@for($j = 1; $j <= $i; $j++)/{{ Request::segment($j) }}@endfor">{{ Request::segment($i) }}</a>
-            </li>
-        @endfor
+        <div class="jumbotron">
+            <h2>About us?</h2>
+            <p>
+                We are a group of Computer Science 383 students at the University of Idaho tasked with creating a collaborative IDE.
+            </p>
+        </div>
     </ul>
 @endsection
 
