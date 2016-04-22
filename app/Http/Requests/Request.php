fafad40f01f3d89f720f82437470235b42ffefe2
brec9824 <brec9824@vandals.uidaho.edu>
2016-04-22 00:17:32 -0400

    Fixed forbidden page
    Overloaded the forbidden handler to bring up the 403 page. Also edited it so if the user is not logged in and clicks submit comment the loggin window mini window pops up.

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+    public function forbiddenResponse()
+    {
+        return response()->view('errors.403');
+    }
 }
