f775126b2f7f36ea4c0aff0562e5cfceb6ea1562
John Snevily <snev7821@vandals.uidaho.edu>
2016-04-22 17:21:22 -0700

    Eric and John's attempt to correct new settings page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 
-use App\Http\Requests;
 
-class SettingsController extends Controller
-{
-=======
 use App\Http\Requests;
 use App\Settings;
 
 class SettingsController extends Controller
 {
+
+    
     //DO we need this? What's the difference between this and the next function?
     protected function create(array $data)
     {
@@ -25,18 +22,12 @@ class SettingsController extends Controller
 
         ]);
     }
-
-<<<<<<< HEAD
-    
-
     
     
->>>>>>> 16b05e60849177353837a729893f03a10660946a
-    public function editSettings()
-=======
+    
     //TO be used in Authentication - whenever a new user is created, call this function
     /*public function initializeSettings()
->>>>>>> 2c32be178e9c2a5b564c7b719b759a7a99bd7ede
+
     {
         $newEntry = Settings::create([
             'user_id' => Auth::user()->user_id,
--- app/Settings.php
+++ app/Settings.php
@@ -1,37 +1,27 @@
 <?php
 
-<<<<<<< HEAD
 namespace App\providers;
-=======
-namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
->>>>>>> 2c32be178e9c2a5b564c7b719b759a7a99bd7ede
+
 
 
 
 class Settings extends Model
 {
-<<<<<<< HEAD
     protected $fillable = [
-            'nickname', 'chat_font', 'chat_color', 'enable_chat',
+            'nickname', 'enable_comments',
     ];
-=======
     /**
      * Retrieve user settings
      */
-
-    protected $fillable = [
-        'show_comments',
-    ];
+    
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }    
 
-
     
->>>>>>> 2c32be178e9c2a5b564c7b719b759a7a99bd7ede
 }
