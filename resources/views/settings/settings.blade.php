77403d2369969ad12b542413828fb314e2c4cff1
John Snevily <snev7821@vandals.uidaho.edu>
2016-04-23 21:20:44 -0700

    More pretty tables on the settings page

--- resources/views/settings/settings.blade.php
+++ resources/views/settings/settings.blade.php
@@ -15,27 +15,22 @@
                 <fieldset>
                     <legend>Legend</legend>
                     <div class="form-group">
-                        <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                        <label for="inputEmail" class="col-lg-2 control-label">Nickname</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="inputEmail" placeholder="Email" type="text">
+                            <input class="form-control" id="inputEmail" placeholder="Nickname" type="text">
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="inputPassword" class="col-lg-2 control-label">Password</label>
-                        <div class="col-lg-10">
-                            <input class="form-control" id="inputPassword" placeholder="Password" type="password">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Checkbox
-                                </label>
-                            </div>
-                        </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox"> Enable Comments
+                        </label>
                     </div>
+
                     <div class="form-group">
                         <label for="textArea" class="col-lg-2 control-label">Textarea</label>
                         <div class="col-lg-10">
                             <textarea class="form-control" rows="3" id="textArea"></textarea>
-                            <span class="help-block">A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
+                            <span class="help-block">Write a bio for yourself!.</span>
                         </div>
                     </div>
                     <div class="form-group">
@@ -44,13 +39,13 @@
                             <div class="radio">
                                 <label>
                                     <input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-                                    Option one is this
+                                    Enable Chat
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
                                     <input name="optionsRadios" id="optionsRadios2" value="option2" type="radio">
-                                    Option two can be something else
+                                    Disable Chat
                                 </label>
                             </div>
                         </div>
@@ -66,19 +61,12 @@
                                 <option>5</option>
                             </select>
                             <br>
-                            <select multiple="" class="form-control">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
                         </div>
                     </div>
                     <div class="form-group">
                         <div class="col-lg-10 col-lg-offset-2">
                             <button type="reset" class="btn btn-default">Cancel</button>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary">Apply</button>
                         </div>
                     </div>
                 </fieldset>
