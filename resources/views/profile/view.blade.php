0feadd804cbac08339eb79a995ddabd7e5747b7e
bolt1003 <bolt1003@vandals.uidaho.edu>
2016-04-24 19:15:50 -0400

    Redesigned the CSS on the user profile page

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -6,31 +6,41 @@
 
 @section('mainBody')
     @include('inserts.breadcrumb')
-    <div class="row">
-        <div class="col-md-12">
-            <legend>Profile</legend>
-                <div class="row">
-                <div class="p-col-1-3">
-                    <img class="center-block" scr="images/projects/DefaultProfile.jpg" style="height:175px;width:250px;">
-                <div class="p-center">
-                    <a class="center" class="username-btn">{{ Auth::user()->username }}</a>
-                    <a href="#" class="btn btn-warning">Send Message</a>
-                    <a href="#" class="btn btn-primary">Add To Contact</a>
-                </div>
-                </div>
-                <div class="p-col-2-3">
-                    <h4>Basic Information</h4>
-                    <label>User name:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <label>Birth Date:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <label>Gender:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <h4>Contact Information</h4>
-                    <label>Email:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <label>Phone:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <label>Address:</label><a class="username-btn">{{ Auth::user()->username }}</a>
-                    <h4>About Me</h4>
-            </div>
-            </div>
-	        <!-- Template: http://wrapbootstrap.com/preview/WB09JXK43 -->
-        </div>
+    <div class="container-fluid well">
+        <legend>Profile</legend>
+        <div class="row">
+		    <div class="col-md-2">
+		        <img class="center-block" scr="images/projects/DefaultProfile.jpg">
+		    	<div class="">
+					<h2 class="center"> {{$username}}</h2>
+					<div class="btn-group btn-group-justified">
+			        	<a href="#" class="btn btn-default">Send Message</a>
+			        	<a href="#" class="btn btn-default">Add To Contact</a>
+			    	</div>
+				</div>
+			</div>
+			<div class="col-md-10">
+
+		    	<div class="col-md-6">
+		        	<h4>Basic Information</h4>
+		            <label>User name:</label><p>{{$username}}</p><br/>
+		            <label>Birth Date:</label><p>{{$username }}</p><br/>
+		            <label>Gender:</label><p>{{$username}}</p><br/>
+				</div>
+				<div class="col-md-6">
+		            <h4>Contact Information</h4>
+		            <label>Email:</label><p>{{$username}}</p><br/>
+		            <label>Phone:</label><p>{{$username}}</p><br/>
+		            <label>Address:</label><p>{{$username}}</p><br/>
+				</div>
+				<div class="col-md-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">About Me</div>
+						<div class="panel-body">Panel content</div>
+					</div>	
+				<div/>
+			<div/>
+		<div/>
+        <!-- Template: http://wrapbootstrap.com/preview/WB09JXK43 -->
     </div>
 @stop
