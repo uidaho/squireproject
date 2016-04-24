7b650d5ee7d34b65bb614ac8b7861bf4e0d07c78
John Snevily <snev7821@vandals.uidaho.edu>
2016-04-23 21:15:46 -0700

    Updating pretty tables on the settings page

--- resources/views/settings/settings.blade.php
+++ resources/views/settings/settings.blade.php
@@ -11,65 +11,78 @@
     <main class="primary-main row">
         <section class="grid register-main">
 
-            <section class="col-2-3">
-
-                <div class="mini-background">
-                <h5>{{ Auth::user()->username }}'s settings</h5>
-                    <ul class="join-points">
-                        <li>Wow look at all these settings</li>
-                        <li>Below should be the chat color</li>
-                        <li>Binary Chat enabled? {{ Settings::user()->enable_chat }}</li>
-
-                    </ul>
-                </div>
-
-                <table class="table table-striped table-hover ">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Settings</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Setting 1</td>
-
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Setting 2</td>
-
-                    </tr>
-                    <tr class="info">
-                        <td>3</td>
-                        <td>Setting 3</td>
-
-                    </tr>
-                    <tr class="success">
-                        <td>4</td>
-                        <td>Setting 4</td>
-
-                    </tr>
-                    <tr class="danger">
-                        <td>5</td>
-                        <td>Setting 5</td>
-
-                    </tr>
-                    <tr class="warning">
-                        <td>6</td>
-                        <td>Setting 6</td>
-
-                    </tr>
-                    <tr class="active">
-                        <td>7</td>
-                        <td>Setting 7</td>
-
-                    </tr>
-                    </tbody>
-                </table>
-
-            </section>
+            <form class="form-horizontal">
+                <fieldset>
+                    <legend>Legend</legend>
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                        <div class="col-lg-10">
+                            <input class="form-control" id="inputEmail" placeholder="Email" type="text">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+                        <div class="col-lg-10">
+                            <input class="form-control" id="inputPassword" placeholder="Password" type="password">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Checkbox
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="textArea" class="col-lg-2 control-label">Textarea</label>
+                        <div class="col-lg-10">
+                            <textarea class="form-control" rows="3" id="textArea"></textarea>
+                            <span class="help-block">A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-lg-2 control-label">Radios</label>
+                        <div class="col-lg-10">
+                            <div class="radio">
+                                <label>
+                                    <input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+                                    Option one is this
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input name="optionsRadios" id="optionsRadios2" value="option2" type="radio">
+                                    Option two can be something else
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="select" class="col-lg-2 control-label">Selects</label>
+                        <div class="col-lg-10">
+                            <select class="form-control" id="select">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                            <br>
+                            <select multiple="" class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-lg-10 col-lg-offset-2">
+                            <button type="reset" class="btn btn-default">Cancel</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
 
         </section>
     </main>
