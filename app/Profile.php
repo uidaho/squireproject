a14e036084b460436578a02ed50bdc195820b2f3
bolt1003 <bolt1003@vandals.uidaho.edu>
2016-05-03 22:19:05 -0700

    Updated User Profile Appearance

--- app/Profile.php
+++ app/Profile.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
 	protected $table = 'user_profiles';
-    protected $fillable = ['date_of_birth', 'gender', 'biography', 'first_name', 'last_name','profile_url','phone','address','created_at', 'updated_at'];
+    protected $fillable = ['date_of_birth', 'first_name', 'last_name','profile_url','phone','address', 'gender', 'biography', 'created_at', 'updated_at'];
 
 	public function user()
     {
--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -6,52 +6,56 @@
 
 @section('mainBody')
     @include('inserts.breadcrumb')
-    <div class="container-fluid well">
-        <legend>Profile</legend>
-        <div class="row">
-		    <div class="col-md-2">
-				<img class="center-block" scr={{$user->profile->profile_url}} >
-		    	<div class="">
-					<h2 class="center"> {{$user->username}}</h2>
-					@if ($user->username != Auth::user()->username)
-						<div class="btn-group btn-group-justified">
-			        	<a href="#" class="btn btn-default">Send Message</a>
-			        	<a href="#" class="btn btn-default">Add To Contact</a>
-			    		</div>
-					@endif
+
+    <div class="container-fluid">
+		<ul class="nav nav-tabs r-tab-bottom">
+  			<li class="active"><a href="profile/{{$user->username}}">Profile</a></li>
+  			<li><a href="/profile/projects/{{$user->username}}">Projects</a></li>
+  			@if ($user->username == Auth::user()->username)
+				<li><a href="profile/edit/{{$user->username}}">Edit</a></li>
+			@endif
+		</ul>
+		<div class="panel panel-default" >
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="center-block" scr="{{$user->profile->profile_url}}">
+						<div class="">
+							<h2 class="center"> {{$user->username}}</h2>
+							@if ($user->username != Auth::user()->username)
+								<div class="btn-group btn-group-justified">
+								<a href="#" class="btn btn-default">Send Message</a>
+								<a href="#" class="btn btn-default">Add To Contact</a>
+								</div>
+							@endif
 					
-				</div>
-			</div>
-			<div class="col-md-10">
-		    	<div class="col-md-6">
-		        	<h4>Basic Information</h4>
-		            <label>Name:</label><p>{{$user->profile->first_name}} {{$user->profile->last_name}}</p><br/>
-		            <label>Birth Date:</label><p>{{$user->profile->date_of_birth}}</p><br/>
-		            <label>Gender:</label>
-					@if ($user->profile->gender)<p>Male</p> 
-					@else <p>Female</p> 
-					@endif<br/>
-				</div>
-				<div class="col-md-6">
-		            <h4>Contact Information</h4>
-		            <label>Email:</label><p>{{$user->email}}</p><br/>
-		            <label>Phone:</label><p>{{$user->profile->phone}}</p><br/>
-		            <label>Address:</label><p>{{$user->profile->address}}</p><br/>
-				</div>
-				<div class="col-md-12">
-					<div class="panel panel-default">
-						<div class="panel-heading">About Me</div>
-						<div class="panel-body"><p>{{$user->profile->biography}}</p></div>
-					</div>	
+						</div>
+					</div>
+					<div class="col-md-10">
+						<div class="col-md-6">
+							<h4>Basic Information</h4>
+						    <label>Name:</label><p>{{$user->profile->first_name}} {{$user->profile->last_name}}</p><br/>
+						    <label>Birth Date:</label><p>{{$user->profile->date_of_birth}}</p><br/>
+						    <label>Gender:</label>
+							@if ($user->profile->gender)<p>Male</p> 
+							@else <p>Female</p> 
+							@endif<br/>
+						</div>
+						<div class="col-md-6">
+						    <h4>Contact Information</h4>
+						    <label>Email:</label><p>{{$user->email}}</p><br/>
+						    <label>Phone:</label><p>{{$user->profile->phone}}</p><br/>
+						    <label>Address:</label><p>{{$user->profile->address}}</p><br/>
+						</div>
+						<div class="col-md-12">
+							<div class="panel panel-default">
+								<div class="panel-heading">About Me</div>
+								<div class="panel-body"><p>{{$user->profile->biography}}</p></div>
+							</div>	
+						<div/>
+					<div/>
 				<div/>
-			<div/>
-		<div/>
-			<div>
-				@if ($user->username == Auth::user()->username)
-					<a href="profile/edit/{{$user->username}}">Edit</a>
-				@endif
-				<a href="/profile/projects/{{$user->username}}">User Projects</a>
-			<div/>
-			
-    </div>
+			</div>
+		</div>
+	</div>
 @stop
