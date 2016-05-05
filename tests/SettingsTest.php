57310dba9f749220401f5d1b5f02488fb2984c6b
John Snevily <snev7821@vandals.uidaho.edu>
2016-05-04 19:04:58 -0700

    stubed out settings unit tests #154

---
+++ tests/SettingsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SettingsTest extends TestCase
+{
+    /**
+     * Test the update method in the Settings class.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+
+
+
+    /**
+     * Test the view method in the Settings class.
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->assertTrue(true);
+    }
+
+}
