4bcf1c36f274d0a44bc665e8fbc5ceeb3cf5069a
John Snevily <snev7821@vandals.uidaho.edu>
2016-05-04 19:46:41 -0700

    playing with setting test

--- tests/SettingsTest.php
+++ tests/SettingsTest.php
@@ -25,7 +25,7 @@ class SettingsTest extends TestCase
      */
     public function testView()
     {
-        $this->visit('/settings')
+        $this->visit('settings')
             ->see('Settings')
             ->dontSee('404');
     }
